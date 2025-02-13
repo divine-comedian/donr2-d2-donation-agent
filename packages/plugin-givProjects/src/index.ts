dd78aa11949c0e5194c51d013ceeb4df92f8e44a
kkatusic <kkatusic@gmail.com>
2025-02-13 21:39:47 +0100

    adding giveth projects

---
+++ packages/plugin-givProjects/src/index.ts
@@ -0,0 +1,11 @@
+import { Plugin } from "@elizaos/core";
+// import { getBoostedProjectsAction } from "./actions/getBoostedProjects";
+export const givProjectsPlugin: Plugin = {
+    name: "givProjects",
+    actions: [],
+    description: "GIV Projects plugin",
+    evaluators: [],
+    providers: [],
+};
+
+export default givProjectsPlugin;
